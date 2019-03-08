df199dfa567af658f5c2353cefbe6239ad04b46a
dougbass <dougbass1@yahoo.com>
2019-03-08 11:42:07 -0800

    Add documentation

--- poll.sh
+++ poll.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Poll for new build requests, and kick off the builds
+# Author: Doug Bass
+# Copyright 2019 ConnectALL LLC
+
 . setup.sh
 
-url=http://localhost:8090
-key=62dc142c-3f33-4b95-9713-7ad4c7af9107
 applink=jenkinsv2
 
 if [ ! -f lasttime ]; then
 	touch lasttime
 fi
 
+# Get the last time a poll was run and build the json request
 lastRun="`date -r lasttime "+%Y-%m-%d %H:%M:%S"`.001"
 echo Last run was $lastRun
 
@@ -19,11 +22,12 @@ json="
  \"lastModifiedTime\":\"$lastRun\"
 }"
 
-#2017-08-24 14:30:00.000
+#Date must be in this format: 2017-08-24 14:30:00.000
 
 echo "Send JSON: $json"
 echo "via ${ConnectAllUrl}/api/2/postRecord?apikey=$ConnectAllApiKey"
 
+# Poll for any new build requests
 curl \
 --header "Content-Type: application/json;charset=UTF-8" -X POST \
 -d "$json" \
@@ -32,10 +36,13 @@ curl \
 touch lasttime
 echo
 
+# Parse out the build to run - currently it only finds one build to Run
+# @todo for loop to parse out all the build requests
 workspace=`cat request.json | sed -e 's/[{}]/''/g' | awk -v RS=',"' -F: '/workspace/ {print $2}' | sed -e 's/\"//g'`
 echo
 echo Result is $workspace
 
+# Send the build request to jenkins
 if [ "$workspace" = "" ]; then
 	echo "Nothing to build"
 else
